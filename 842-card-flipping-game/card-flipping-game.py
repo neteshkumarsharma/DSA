80fe4a4139a793193633b276ec1aafe7ae9f34c7
Netesh Kumar Sharma <125692946+neteshkumarsharma@users.noreply.github.com>
2026-02-23 13:09:59 +0530

    Time: 1 ms (85.64%) | Memory: 19.4 MB (47.18%) - LeetSync

---
+++ 842-card-flipping-game/card-flipping-game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def flipgame(self, fronts: List[int], backs: List[int]) -> int:
+        same = set()
+        for f, b in zip(fronts, backs):
+            if f == b:
+                same.add(f)
+        
+        result = float('inf')
+        for f in fronts:
+            if f not in same:
+                result = min(result, f)
+        for b in backs:
+            if b not in same:
+                result = min(result, b)
+        
+        return result if result != float('inf') else 0
