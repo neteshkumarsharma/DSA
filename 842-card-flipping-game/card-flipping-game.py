66b9089913735005f82058810e76d912b2a42f32
Netesh Kumar Sharma <125692946+neteshkumarsharma@users.noreply.github.com>
2026-02-23 13:10:25 +0530

    Time: 3 ms (77.95%) | Memory: 19.4 MB (47.18%) - LeetSync

--- 842-card-flipping-game/card-flipping-game.py
+++ 842-card-flipping-game/card-flipping-game.py
@@ -1,6 +1,6 @@
 class Solution:
     def flipgame(self, fronts: List[int], backs: List[int]) -> int:
-        same = set()
+        same = set()#
         for f, b in zip(fronts, backs):
             if f == b:
                 same.add(f)
